5c1667488a0e4e424d0c80c2d3a35d9143557052
masanara <masanara@gmail.com>
2022-02-22 01:01:31 +0000

    update unshare.sh

--- demo/unshare.sh
+++ demo/unshare.sh
@@ -1,5 +1,14 @@
-ROOTFS=$(mktemp -d); echo $ROOTFSROOTFS=$(mktemp -d); echo $ROOTFS
+ROOTFS=$(mktemp -d); echo $ROOTFS
 docker export $(docker create centos:7) | tar xf - -C $ROOTFS
 echo $ROOTFS
 ls –l $ROOTFS
+which python
+which python3
 unshare -uinpUrf --mount-proc chroot $ROOTFS bash
+
+---
+
+mount -t proc proc /proc
+mount -t devpts devpts /dev/pts
+
+python -V
