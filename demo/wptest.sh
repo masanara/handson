349fa9b2207a13f2fbd660b5b04ba402e8824a57
masanara <masanara@gmail.com>
2023-02-08 10:24:21 +0900

    update 20230208

--- demo/wptest.sh
+++
@@ -1,8 +0,0 @@
-#for i in {0..10};\
-for i in {0..20};\
-do \
-#kubectl apply -n test$i -f .
-#kubectl delete deploy -n test$i nginx
-kubectl delete -n test$i -f .
-#kubectl delete -n test$i pvc mysql-data-mysql-0
-done;
